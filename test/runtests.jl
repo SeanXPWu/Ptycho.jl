718e1ebcc78b8b5ec6dffd3312d2b9bb92803a6a
Xiaopeng Wu <xiaopengwu2001@gmail.com>
2023-09-29 23:37:12 +0100

    Fixed index assignment in load_dps

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,15 @@ using Test
 
 @testset "Ptycho.jl" begin
     # Write your tests here.
-    @test Parameters(300, 1.03, 4.65, 31.25, -126, -13500) ==
-          Parameters(300, 1.03, 4.65, ScanTrajectory(31.25, -126), AberrationParameters(-13500))
-    @test init_probe(Parameters(300, 1.03, 4.65, 31.25, -126, -13500),DiffractionPatterns(load_dp("../test_data/dp_1.mat","dp"))).ProbeMatrix == load_dp("../probe.mat","probe")
+    @test Parameters(300, 1.03, 4.65, 31.25, -126, -13500) == Parameters(
+        300,
+        1.03,
+        4.65,
+        ScanTrajectory(31.25, -126),
+        AberrationParameters(-13500),
+    )
+    @test init_probe(
+        Parameters(300, 1.03, 4.65, 31.25, -126, -13500),
+        DiffractionPatterns(load_dp("../test_data/dp_1.mat", "dp")),
+    ).ProbeMatrix == load_dp("../probe.mat", "probe")
 end
