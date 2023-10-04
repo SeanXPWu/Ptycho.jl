a8fda54538fabdcb5478fe4a99f149d73ae5a82d
Yu_Lei <799040451@qq.com>
2023-10-04 16:54:14 +0100

    rmse fix

--- src/Reconstruction.jl
+++ src/Reconstruction.jl
@@ -173,7 +173,7 @@ function prestart(params::Parameters, dps::DiffractionPatterns)
 end
 
 function rmse(arr1::T1, arr2::T2) where {T1<:AbstractArray, T2<:AbstractArray}
-    return sqrt(sum((arr1 .- arr2) .^ 2))
+    return sqrt(sum((arr1 .- arr2) .^ 2)/length(arr1))
 end
 
 function iterate(recon::Reconstruction,trans_exec,params::Parameters, dps::DiffractionPatterns, dpList, List_index)
